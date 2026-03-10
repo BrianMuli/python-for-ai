2bfba634004cf5595b77dc76cc54547b536c8d65
Brian Muli <mulibrian9169@gmail.com>
2026-03-10 13:19:18 +0300

    initial commit

---
+++ get_data.py
@@ -0,0 +1,7 @@
+class Dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+
+dog1=Dog("jerry","German shephered")
+print(dog1.name)   
---
+++ sales-analysis/output/helpers.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df=pd.read_csv("sales-analysis/data/sales.csv")
+df.head()
+df.info()
