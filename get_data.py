8af5cfa6600fc398b22e08a9e708feb043e3dda4
Brian Muli <mulibrian9169@gmail.com>
2026-06-19 12:38:21 +0300

    new venv

--- get_data.py
+++ get_data.py
@@ -1,11 +1,2 @@
-#creating classe
-class Dog:
-    def __init__(self,name,breed):
-        self.name=name
-        self.breed=breed
-    def bark(self):
-        print("Bark!!!Bark!!")    
 
-dog1=Dog("jetty","german")
-dog1.bark()
-dog1.name
+print("how are we")
--- hello.py
+++ hello.py
@@ -1,22 +1,3 @@
-import requests
+print("hello world here it iss")
 
-def get_temperature(latitude, longitude):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
-    response = requests.get(url)
-    data = response.json()
-    return data['hourly_units']['temperature_2m']
 
-# call it in the same cell to test
-print(get_temperature(48.35, 2.35))
-import matplotlib as plt
-
-import pandas as pd
-s=pd.Series([10,30,20,45,84,551,71,22,55,44,45])
-s.name="scores"
-print(scores)
-s.std()
-s.describe()
-
-import sqlite3 
-conn=sqlite3.connect("dvdrental.db")
-df=pd.read_sql("SELECT*FROM dvdrental",conn)
